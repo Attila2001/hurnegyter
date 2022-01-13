6d710cdbcb8bc2fc71152ed5dac4b5e1fe0e1f77
unknown <npwilde6@gmail.com>
2022-01-13 10:27:25 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,12 @@
+/*
+* File: app.component.ts
+* Author: jakab attila
+* Copyright: 2022, jakab attila
+* Group: Szoft V
+* Date: 2022-01-13
+* Github: https://github.com/Attila2001
+* Licenc: GNU GPL
+*/
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +14,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hurnegyter';
-}
+  export class AppComponent {
+    title = 'hurnegyter';
+    A: number;
+    B: number;
+    C: number;
+    D: number;
+    terulet: number;
+    constructor(){
+        this.A = 0;
+        this.B = 0;
+        this.C = 0;
+        this.D = 0;
+        this.terulet = 0;
+    }
+    calcArea(): any{
+      console.log(this.terulet);
+      this.terulet = Number(this.A) + Number(this.B) + Number(this.C) + Number(this.D) /2;
+      
+    }
+  }
+
+
+
